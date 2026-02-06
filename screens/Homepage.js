c6100649d564f1bb1527c225138d06d36a3feef9
Shivam Saxena <shivamsaxena862006@gmail.com>
2026-02-06 15:04:04 +0530

    styling fix on home page

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -314,6 +314,7 @@ function TeamCard({ navigation, branchCoords }) {
                     data={{ item: item.originalData }}
                     navigation={navigation}
                     branchCoords={branchCoords}
+                    compact
                   />
                 ) : (
                   <OngoingEventCard
