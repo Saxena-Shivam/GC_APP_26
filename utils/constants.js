d10dae7a28a62b43569c9b5e1a03c77ea5e5c08c
Shivam Saxena <shivamsaxena862006@gmail.com>
2026-02-12 02:07:12 +0530

    Add google-services.json and update backend URL

--- utils/constants.js
+++ utils/constants.js
@@ -2,5 +2,5 @@
 // export const backend_link = "https://dzvv76kj-3002.inc1.devtunnels.ms/";
 // export const backend_link = "https://bwb6rp8h-3002.inc1.devtunnels.ms/";
 //export const backend_link = "https://gc2024iitbbs.onrender.com/";
-export const backend_link = "https://fzdpjgck-3002.inc1.devtunnels.ms/";
+export const backend_link = "https://gc-app-26-backend.onrender.com";
 // export const backend_link = "https://bwb6rp8h-3002.inc1.devtunnels.ms/"
